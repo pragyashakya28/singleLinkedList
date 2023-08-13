d149a21188c9bf886b37dbeb45348189a68bf04c
pragyashakya28 <pragya_s@cs.iitr.ac.in>
2023-08-14 00:24:07 +0530

    array commit2

--- array.cpp
+++ array.cpp
@@ -1,119 +1,159 @@
-#include <iostream>
-using namespace std;
+#include<iostream>
 
-template<typename T, size_t N>
+template <typename T>
 class Array {
 private:
-    T arr[N];
-    size_t size;
+    T* arr;
+    int capacity;
+    int size;
+    int frontIndex;
 
 public:
-    Array() {
-        size = 0;
+    Array(int cap) : capacity(cap), size(0), frontIndex(0) {
+        arr = new T[capacity];
     }
 
-    
-    void addElement(T value, size_t position) {
-        if (size < N && position <= size) {
-            for (size_t i = size; i > position; i--) {
-                arr[i] = arr[i - 1];
-            }
-            arr[position] = value;
-            size++;
+    ~Array() {
+        delete[] arr;
+    }
+
+    void push(T data) {
+        if (size < capacity) {
+            arr[size++] = data;
         } else {
-            cout << "Invalid position or array is full. Cannot insert the element." << endl;
+            std::cout << "Array is full." << std::endl;
         }
     }
 
-    
-    void removeElement(size_t position) {
-        if (position < size) {
-            for (size_t i = position; i < size - 1; i++) {
-                arr[i] = arr[i + 1];
-            }
-            size--;
-        } else {
-            cout << "Invalid position. Cannot delete the element." << endl;
-        }
-    }
-
-    
-    int search(T value) {
-        for (size_t i = 0; i < size; i++) {
-            if (arr[i] == value) {
-                return static_cast<int>(i);
+    void pop(T data) {
+        int index = -1;
+        for (int i = 0; i < size; ++i) {
+            if (arr[i] == data) {
+                index = i;
+                break;
             }
         }
-        return -1; 
-    }
 
-    
-    void display() {
-        if (size == 0) {
-            cout << "Array is empty." << endl;
+        if (index == -1) {
+            std::cout << "Element not found." << std::endl;
             return;
         }
 
-        cout << "Array elements: ";
-        for (size_t i = 0; i < size; i++) {
-            cout << arr[i] << " ";
+        for (int i = index; i < size - 1; ++i) {
+            arr[i] = arr[i + 1];
         }
-        cout << endl;
+        --size;
+    }
+
+    T peek() {
+        if (size > 0) {
+            return arr[size - 1];
+        } else {
+            std::cout << "Array is empty." << std::endl;
+            return T(); // Return a default value for the data type
+        }
+    }
+
+	
+     
+    void enqueue(T data) {
+        if (size < capacity) {
+            arr[(frontIndex + size) % capacity] = data;
+            ++size;
+        } else {
+            std::cout << "Array is full." << std::endl;
+        }
+    }
+
+    void dequeue() {
+        if (size > 0) {
+            frontIndex = (frontIndex + 1) % capacity;
+            --size;
+        } else {
+            std::cout << "Array is empty." << std::endl;
+        }
+    }
+
+    T front() 
+    {
+        if (size > 0) {
+            return arr[frontIndex];
+        } else {
+            std::cerr << "Array is empty." << std::endl;
+            return T(); // Return a default value for the data type
+        }
+    }
+     	
+	
+    bool isEmpty() {
+        return (size == 0);
+    }
+
+    void display() {
+        for (int i = 0; i < size; ++i) {
+            std::cout << arr[(frontIndex + i) % capacity] << " ";
+        }
+        std::cout << std::endl;
     }
 };
 
-int main() {
-    const size_t arraySize = 10;
-    Array<int, arraySize> arr;
 
-    int choice, value;
-    size_t position;
 
-    while (true) {
-        cout << "\nArray Operations:\n";
-        cout << "1. Insert\n";
-        cout << "2. Delete\n";
-        cout << "3. Search\n";
-        cout << "4. Print\n";
-        cout << "5. Exit\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
 
-        switch (choice) {
-            case 1:
-                cout << "Enter the value to insert: ";
-                cin >> value;
-                cout << "Enter the position to insert: ";
-                cin >> position;
-                arr.addElement(value, position);
-                break;
-            case 2:
-                cout << "Enter the position to delete: ";
-                cin >> position;
-                arr.removeElement(position);
-                break;
-            case 3:
-                cout << "Enter the value to search: ";
-                cin >> value;
-                position = arr.search(value);
-                if (position != -1) {
-                    cout << "Element found at position " << position << "." << endl;
-                } else {
-                    cout << "Element not found in the array." << endl;
-                }
-                break;
-            case 4:
-                arr.display();
-                break;
-            case 5:
-                cout << "Exiting the program.\n";
-                return 0;
-            default:
-                cout << "Invalid choice. Please try again.\n";
-                break;
-        }
-    }
 
-    return 0;
-}
+
+//int main() {
+  //  const size_t arraySize = 10;
+    //Array<int, arraySize> arr;
+
+   // int choice, value;
+    //size_t position;
+
+   // while (true) {
+     //   cout << "\nArray Operations:\n";
+     //   cout << "1. Insert\n";
+     //   cout << "2. Delete\n";
+     //   cout << "3. Search\n";
+     //   cout << "4. Print\n";
+     //   cout << "5. Exit\n";
+     //   cout << "Enter your choice: ";
+     //   cin >> choice;
+
+      //  switch (choice) {
+        //    case 1:
+        //        cout << "Enter the value to insert: ";
+        //        cin >> value;
+        //        cout << "Enter the position to insert: ";
+        //        cin >> position;
+        //        arr.addElement(value, position);
+        //        break;
+        //    case 2:
+        //        cout << "Enter the position to delete: ";
+        //        cin >> position;
+        //        arr.removeElement(position);
+        //        break;
+        //    case 3:
+        //        cout << "Enter the value to search: ";
+        //        cin >> value;
+        //        position = arr.search(value);
+        //        if (position != -1) {
+        //            cout << "Element found at position " << position << "." << endl;
+        //        } else {
+        //            cout << "Element not found in the array." << endl;
+        //        }
+        //        break;
+        //    case 4:
+        //        arr.display();
+        //        break;
+        //    case 5:
+        //        cout << "Exiting the program.\n";
+        //        return 0;
+        //    default:
+        //        cout << "Invalid choice. Please try again.\n";
+        //        break;
+       // }
+   // }
+
+  //  return 0;
+//}
 
