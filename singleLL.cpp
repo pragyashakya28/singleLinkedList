e77f75d76da791b5978deb940aa33b43bc1b0b57
pragyashakya28 <pragya_s@cs.iitr.ac.in>
2023-08-13 20:02:51 +0530

    stackLL commit

--- singleLL.cpp
+++ singleLL.cpp
@@ -30,6 +30,12 @@ class LinkedList
 	 
 	}
 
+	void insert(T val){
+		Node<T>* newnode = new Node<T>(val);
+		newnode->next = head;
+		head = newnode;
+	}
+
 	void insertAtPosition(T val, int position ){
 		Node<T>* newnode = new Node<T>(val);
 
@@ -54,103 +60,142 @@ class LinkedList
 		newnode->next=current;
 
 	}
+	
+	//For Stack
+	void removeAtTop() {
+	
+		cout<<"Deleted value: "<<head->data<<endl;
+        	if (isEmpty()) {
+            		cout << "List is empty.";
+            		return;
+        	}
 
-	bool deleteNode(int index)
-	{
-		
+        	Node<T>* temp = head;
+        	head = head->next;
+        	delete temp;
+    	}
+	
+	
+	T peek(){
 		if(head == NULL){
-			cout<<"list is empty";
-			return false;
-	        }
-
-		if(index == 0){
-			Node<T>* temp= head;
-			head = head->next;
-			delete temp;
-			return true;
+			cout<<"list is empty"<<endl;
+			return T();
 		}
-		Node<T>* prev = NULL;
-		Node<T>* curr = head;
-		int currpos=0;
-		
-		while(curr!= NULL && currpos < index){
-			prev = curr;
-			curr=curr->next;
-			currpos++;
-		}
+		 return head->data;
+	}
+	
+	//For Queue
+	void removeAtFront() {
+        	if (head == nullptr) {
+            		std::cout << "List is empty." << std::endl;
+            		return;
+        	}
 
-		if(curr == NULL){
-			return false;
-		}
+       	 	Node<T>* current = head;
+        	Node<T>* prev = nullptr;
 
-		prev->next = curr->next;
-		delete curr;
-		return true;
-	}	
+        	while (current->next != nullptr) {
+            		prev = current;
+            		current = current->next;
+        	}
+
+        	if (prev != nullptr) {
+            		prev->next = nullptr;
+        	} else {
+            	head = nullptr;
+        	}
+
+        	delete current;
+    	}
 	
 
-	void printNode(){
-		Node<T>* temp = head;
-		if(head == NULL){
-			cout<<"list is empty";
-		}
-		while(temp!=NULL){
-			cout<<temp->data<<"-> ";
-			temp = temp->next;
-		}
-		if(temp == NULL && head!=NULL){ cout<<"NULL"; }
+	T front() {
+        	if (head == nullptr) {
+        	    std::cout << "List is empty." << std::endl;
+        	    return T(); // Return a default value for the data type
+        	}
+
+        	Node<T>* current = head;
+        	while (current->next != nullptr) {
+        	    current = current->next;
+        	}
+
+        	return current->data;
+    	}
+
+	
+	bool isEmpty(){
+		return(head==NULL);
 	}
 
+	void display() {
+        	Node<T>* current = head;
+        	while (current != nullptr) {
+            	std::cout << current->data << " -> ";
+            	current = current->next;
+        	}
+        	std::cout << "nullptr" << std::endl;
+    	}
+
+    	~LinkedList() {
+        	Node<T>* current = head;
+        	while (current != nullptr) {
+        	    Node<T>* temp = current;
+        	    current = current->next;
+        	    delete temp;
+        	}
+    	}
 
 };
 
-int main()
-{
-	LinkedList<int>  link;
- 	int choice, pos, insertpos;
-	int value;
+//int main()
+//{
+//	LinkedList<int>  link;
+// 	int choice, pos, insertpos;
+//	int value;
+//
+//        do{
+//		cout<<"1. Insert element\n"
+//			<<"2. Delete element\n"
+//			<<"3. Display list\n"
+//			<<"4. Exit\n"
+//			<<"Enter your choice: ";
+//		cin>>choice;
+//		switch(choice){
+//			case 1: 
+//				cout<<"Enter value to be inserted: ";
+//				cin>>value;
+//				cout<<"Enter position of node: ";
+//				cin>>insertpos;
+//				link.insertAtPosition(value,insertpos);
+//				break;
+//			case 2:
+//				cout<<"Enter position of node to be deleted: ";
+//				cin>>pos;
+//				if(link.deleteNode(pos)){
+//					cout<<"node at position "<<pos<<" deleted successfully\n";
+//				}
+//				else{
+//					cout<<"position "<<pos<<" is out of range\n";
+//				}
+//				
+//
+//				break;
+//			case 3:
+//				cout<<"Single Linked List: ";
+//				link.printNode();
+//				cout<<endl;
+//				break;
+//			case 4:
+//				cout<<"Exiting program \n";
+//				break;
+//			default: 
+//				cout<<"Invalid choice. Try again. \n";
+//		}
+//	}while(choice!=4);	
 
-        do{
-		cout<<"1. Insert element\n"
-			<<"2. Delete element\n"
-			<<"3. Display list\n"
-			<<"4. Exit\n"
-			<<"Enter your choice: ";
-		cin>>choice;
-		switch(choice){
-			case 1: 
-				cout<<"Enter value to be inserted: ";
-				cin>>value;
-				cout<<"Enter position of node: ";
-				cin>>insertpos;
-				link.insertAtPosition(value,insertpos);
-				break;
-			case 2:
-				cout<<"Enter position of node to be deleted: ";
-				cin>>pos;
-				if(link.deleteNode(pos)){
-					cout<<"node at position "<<pos<<" deleted successfully\n";
-				}
-				else{
-					cout<<"position "<<pos<<" is out of range\n";
-				}
-				
+//	return 0;
 
-				break;
-			case 3:
-				cout<<"Single Linked List: ";
-				link.printNode();
-				cout<<endl;
-				break;
-			case 4:
-				cout<<"Exiting program \n";
-				break;
-			default: 
-				cout<<"Invalid choice. Try again. \n";
-		}
-	}while(choice!=4);	
-
-	return 0;
-}
+//}
 
 
